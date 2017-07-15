fa145ea6d7131ee05e88b717055d099bebf3cc27
Mike Doyle <mikedoyle007@gmail.com>
2017-07-14 17:16:16 -0700

    Components will render to the screen - dummy info

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,8 @@
 angular.module('video-player')
-
+  .controller('appCtrl', function($scope) {
+    $scope.$ctrl.videos = exampleVideoData;
+  })
   .component('app', {
     templateUrl: 'src/templates/app.html',
+    controller: 'appCtrl'
   });
