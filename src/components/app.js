2a405d808cc3a034333f057d841a04bc1c35ff12
Mike Doyle <mikedoyle007@gmail.com>
2017-07-14 17:18:17 -0700

    Have a searchResults function on the scope

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ angular.module('video-player')
   .controller('appCtrl', function($scope) {
     $scope.$ctrl.videos = exampleVideoData;
     $scope.$ctrl.selectVideo = function() {};
+    $scope.$ctrl.searchResults = function() {};
   })
   .component('app', {
     templateUrl: 'src/templates/app.html',
